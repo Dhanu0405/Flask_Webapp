ee79cc6e89c75e59c55408b280094d62e6521f76
DHANUSH RAJESH <148962990+Dhanu0405@users.noreply.github.com>
2024-05-26 14:12:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,23 +30,23 @@ def home():
         create = request.form.get("create", False)
 
         if not name:
-            return render_template("home.html", error="Please enter a name.", code=code, name=name)
+            return render_template("index.html", error="Please enter a name.", code=code, name=name)
 
         if join != False and not code:
-            return render_template("home.html", error="Please enter a room code.", code=code, name=name)
+            return render_template("index.html", error="Please enter a room code.", code=code, name=name)
         
         room = code
         if create != False:
             room = generate_unique_code(4)
             rooms[room] = {"members": 0, "messages": []}
         elif code not in rooms:
-            return render_template("home.html", error="Room does not exist.", code=code, name=name)
+            return render_template("index.html", error="Room does not exist.", code=code, name=name)
         
         session["room"] = room
         session["name"] = name
         return redirect(url_for("room"))
 
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route("/room")
 def room():
@@ -54,7 +54,7 @@ def room():
     if room is None or session.get("name") is None or room not in rooms:
         return redirect(url_for("home"))
 
-    return render_template("room.html", code=room, messages=rooms[room]["messages"])
+    return render_template("index.html", code=room, messages=rooms[room]["messages"])
 
 @socketio.on("message")
 def message(data):
